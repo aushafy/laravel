ead215ce1606aeed56f3886de6efdc18b42f9ccb
Aushafy <42592443+aushafy@users.noreply.github.com>
2020-10-10 09:53:35 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Aushafy
                 </div>
 
                 <div class="links">
