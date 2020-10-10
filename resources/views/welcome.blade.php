e15ea72ecda65e0fd89683764457b0503841b4c5
aushafy <aushafy@gmail.com>
2020-10-10 10:10:18 +0700

    tambahin judul

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Aushafy
+                    Laravel Production
                 </div>
 
                 <div class="links">
